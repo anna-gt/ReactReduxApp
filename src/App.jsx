6189550007efb02930da502e463b1056fc42099a
Anna Hetsman <125794946+anna-gt@users.noreply.github.com>
2024-04-28 18:14:19 +0300

    change basename

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import './App.less'
 function App() {
 
   return (
-     <BrowserRouter basename={process.env.PUBLIC_URL}>
+     <BrowserRouter basename={window.location.pathname || ''}>
 		 	<div className='container'>
 				<Routes>
 					<Route exact path="/" element={<Main />} />
